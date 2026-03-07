dc7e2c4b8b066d53097e9d1a2c1f3e05be076f71
joshjms <joshjms1607@gmail.com>
2026-03-08 00:35:22 +0800

    remove init logs
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -39,27 +39,20 @@ void kmain(void) {
     }
 
     serial_init();
-    printk("Serial initialized.\n");
 
     gdt_init();
-    printk("gdt_init() done.\n");
 
     pmm_init();
-    printk("pmm_init() done.\n");
 
     vmm_init();
-    printk("vmm_init() done.\n");
 
     heap_init();
-    printk("heap_init() done.\n");
 
     idt_init();
-    printk("idt_init() done.\n");
 
     __asm__ volatile ("sti");
 
     pic_init();
-    printk("pic_init() done.\n");
 
     if(!hhdm_request.response) {
         hcf();
@@ -74,23 +67,18 @@ void kmain(void) {
     // Map the APIC first
     vmm_map(kernel_pagemap, hhdm + 0xFEE00000, 0xFEE00000, PTE_WRITABLE | PTE_NX | PTE_PWT | PTE_PCD);
     lapic_init(hhdm + 0xFEE00000);
-    printk("lapic_init() done.\n");
 
     // Map the IOAPIC first
     vmm_map(kernel_pagemap, hhdm + 0xFEC00000, 0xFEC00000, PTE_WRITABLE | PTE_NX | PTE_PWT | PTE_PCD);
     ioapic_init(hhdm + 0xFEC00000);
-    printk("ioapic_init() done.\n");
 
     register_interrupt_handlers();
 
     ps2_init();
-    printk("ps2_init() done.\n");
 
     __asm__ volatile ("sti");
-    printk("Enabled interrupts\n");
 
     lapic_timer_init(0x20, 10);
-    printk("lapic timer set up at 100Hz!\n");
 
     // Ensure we got a framebuffer.
     if (framebuffer_request.response == NULL
